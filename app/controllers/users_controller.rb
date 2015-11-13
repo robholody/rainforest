0bcc0493d485f11860290bcdfcb04ea7ff21c8ed
Rob Holody <robholody@Robs-MacBook-Pro.local>
2015-11-13 00:43:34 -0500

    Adding user names

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,7 +13,7 @@ class UsersController < ApplicationController
 end
   private
   def user_params
-  	params.require(:user).permit(:email, :password, :password_confirmation)
+  	params.require(:user).permit(:email, :password, :password_confirmation, :name)
   end
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,10 @@
 class User < ActiveRecord::Base
+
+	has_many :reviews
+	has_many :products, through: :reviews
+
+
 	has_secure_password
+
+	validates :name, presence: true
 end
